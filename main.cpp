26195931149ef9a7705648d767ba3de1621ba786
Tomáš Milet <imilet@fit.vutbr.cz>
2026-03-18 18:58:29 +0100

    added SDL

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
+#include<SDL3/SDL.h>
 
 int main(int argc,char*argv[]){
+
+  SDL_CreateWindow("IZG2026",1024,768,0);
+  for(;;);
 
   std::cerr << "Papoušek je ožralý, ale hodný!" << std::endl;
   return 0;
